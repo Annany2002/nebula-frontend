46862b493ee7dde75116a7ba7d1b99b3acadb5a4
Annany <annivish2002@gmail.com>
2025-04-15 23:11:01 +0530

    Revert backend URL assignment in App component to use production environment variable, while commenting out the development URL for clarity.

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ import SignUp from "./pages/SignUp";
 import { Toaster } from "@/components/ui/sonner";
 import SingleTable from "./pages/SingleTable";
 
-// export const url = import.meta.env.VITE_BACKEND_URL as string;
-export const url = import.meta.env.VITE_DEV_BACKEND_URL as string;
+export const url = import.meta.env.VITE_BACKEND_URL as string;
+// export const url = import.meta.env.VITE_DEV_BACKEND_URL as string;
 
 const App = () => (
   <ThemeProvider defaultTheme="system">
